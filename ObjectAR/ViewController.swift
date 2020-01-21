106012b19b8b101283a46b4621a44dec3423a91a
Damian <damian.jahn@icloud.com>
2020-01-21 18:28:17 +0100

    Initial Commit

---
+++ ObjectAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectAR
+//
+//  Created by Damian Jahn on 21/01/2020.
+//  Copyright © 2020 Damian Jahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
